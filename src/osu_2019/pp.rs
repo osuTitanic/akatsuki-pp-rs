0dda6c7546cf8334d4a0ebc34478ab088e279450
kanaarima <137962405+kanaarima@users.noreply.github.com>
2023-11-27 15:59:11 +0000

    Nerf speed length bonus

--- src/osu_2019/pp.rs
+++ src/osu_2019/pp.rs
@@ -383,7 +383,7 @@ impl<'m> OsuPP<'m> {
             (5.0 * (attributes.speed_strain as f32 / 0.0675).max(1.0) - 4.0).powi(3) / 100_000.0;
 
         // Longer maps are worth more
-        let len_bonus = 0.88
+        let len_bonus = 1.0
             + 0.4 * (total_hits / 2000.0).min(1.0)
             + (total_hits > 2000.0) as u8 as f32 * 0.5 * (total_hits / 2000.0).log10();
         speed_value *= len_bonus;
